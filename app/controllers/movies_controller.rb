e87cc5bafc832f7f63c4ce71b02058c1668b056a
Salvatore Strangio <sal.strangio@gmail.com>
2022-02-22 20:29:13 +0100

    Almost there

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,26 @@
 class MoviesController < ApplicationController
+
   def new
-    @movie = Movie.new
+    if params[:search]
+      query = params[:search][:title]
+      @movies = Movie.search(query)
+      redirect_to movies_path
+    else
+      @movies = ""
+    end
   end
 
-  def create
-    @movie = Movie.search(query)
-    @movie.save
-  end
-
-  # private
-  # def movie_params
-  #   params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  # def create
+  #   @movie = Movie.search(params[:search][:title])
+  #   @movie.save
   # end
+
+  def index
+    @movies = Movie.search(params[:search][:title])
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating, :search)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,17 +4,17 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true
   validates :title, :overview, presence: true
 
-  def search(query)
+  def self.search(query)
     require 'json'
     require 'open-uri'
 
-    url = ENV['TMBB_BASE_API']+'&query='+query
+    url = ENV['TMBB_BASE_API']+'&query='+query.to_s
     movies_serialized = URI.open(url).read
     movies = JSON.parse(movies_serialized)
     movies['results'].each do |m|
-    Movie.create(title: m['title'], overview: m['overview'],
-                poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2#{m['poster_path']}",
-                rating: m['vote_average'])
+      Movie.create(title: m['title'], overview: m['overview'],
+        poster_url: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2#{m['poster_path']}",
+        rating: m['vote_average'])
     end
   end
 end
