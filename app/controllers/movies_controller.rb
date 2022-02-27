209b903d21c4371549b5e4ccec8071d85b910d60
Salvatore Strangio <sal.strangio@gmail.com>
2022-02-27 15:46:04 +0100

    fixed passed param

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,30 @@
 class MoviesController < ApplicationController
 
   def new
+    if params[:passed_parameter]
+      list_id = params[:passed_parameter][:list_id]
+    else
+      list_id = params[:search][:list_id]
+    end
+    @list = List.find(list_id)
     if params[:search]
       query = params[:search][:title]
       @movies = Movie.search(query)
-      redirect_to movies_path(passed_parameter: params[:search][:title])
+      redirect_to movies_path(passed_parameter: {query: params[:search][:title], list: list_id})
     else
       @movies = ""
     end
   end
 
   def index
-    query = params[:passed_parameter].gsub(/[[:punct:]]/, '%')
+    list_id = params[:passed_parameter][:list]
+    @list = List.find(list_id)
+    query = params[:passed_parameter][:query].gsub(/[[:punct:]]/, '%')
     @movies = Movie.where("lower(replace(title, '-', '')) LIKE lower(?)", "%#{query}%").order('popularity DESC')
   end
 
   private
   def movie_params
-    params.require(:movie).permit(:title, :overview, :poster_url, :rating, :search)
+    params.require(:movie).permit(:id, :title, :overview, :poster_url, :rating, :search)
   end
 end
